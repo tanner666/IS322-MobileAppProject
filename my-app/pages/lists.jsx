d296c04e871056234f6fa03033076ce83029d6eb
tanner666 <tjg5@njit.edu>
2023-12-01 03:50:32 -0500

    implemented add listGroup feature

--- my-app/pages/lists.jsx
+++ my-app/pages/lists.jsx
@@ -2,8 +2,34 @@ import Head from 'next/head'
 import Link from 'next/link'
 import ListGroup from '@/components/listGroup'
 import Navbar from '@/components/navbar'
+import React, { useState, useEffect} from 'react';
 
 export default function Lists(){
+    const [listGroups, setListGroups] = useState([
+        { id: '1', title: 'Grocery List' },
+        { id: '2', title: 'To-Do' },
+        { id: '3', title: 'Chores' },
+    ]);
+
+    // Load list groups from localStorage when component mounts
+    useEffect(() => {
+        const savedListGroups = localStorage.getItem('listGroups');
+        if (savedListGroups) {
+            setListGroups(JSON.parse(savedListGroups));
+        }
+    }, []);
+
+    // Update localStorage whenever the listGroups state changes
+    useEffect(() => {
+        localStorage.setItem('listGroups', JSON.stringify(listGroups));
+    }, [listGroups]);
+
+    const addNewListGroup = () => {
+        const newId = String(Date.now()); // Generate a unique ID based on current time
+        const newListGroup = { id: newId, title: `New List ${listGroups.length + 1}` };
+        setListGroups([...listGroups, newListGroup]);
+    };
+
     return(
         <>
             <Head>
@@ -20,23 +46,25 @@ export default function Lists(){
 
             <div className="mt-2 ml-4 font-dm text-[30px] font-bold flex items-center">
                 My Lists
-                <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39" fill="none">
-                    <circle cx="19.5" cy="19.5" r="19.5" fill="url(#paint0_linear_7_115)"/>
-                    <path d="M13 19.5H26" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M19.5 26V13" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-                    <defs>
-                    <linearGradient id="paint0_linear_7_115" x1="19.5" y1="0" x2="19.5" y2="39" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#5D8EFF"/>
-                    <stop offset="1" stop-color="#3E6FE1"/>
-                    </linearGradient>
-                    </defs>
-                </svg>
+                <button onClick={addNewListGroup} className="ml-4">
+                    <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39" fill="none">
+                        <circle cx="19.5" cy="19.5" r="19.5" fill="url(#paint0_linear_7_115)"/>
+                        <path d="M13 19.5H26" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M19.5 26V13" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                        <defs>
+                        <linearGradient id="paint0_linear_7_115" x1="19.5" y1="0" x2="19.5" y2="39" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#5D8EFF"/>
+                        <stop offset="1" stop-color="#3E6FE1"/>
+                        </linearGradient>
+                        </defs>
+                    </svg>
+                </button>
             </div>
 
             <div className="mt-4 ml-4">
-                <ListGroup groupId="1" defaultGroupTitle="Grocery List" />
-                <ListGroup groupId="2" defaultGroupTitle="To-Do"/>
-                <ListGroup groupId="3" defaultGroupTitle="Chores" />
+                {listGroups.map(listGroup => (
+                    <ListGroup key={listGroup.id} groupId={listGroup.id} defaultGroupTitle={listGroup.title} />
+                ))}
             </div>
             <Navbar/>
         </>
